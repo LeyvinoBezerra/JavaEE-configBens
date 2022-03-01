9a0804ab8ba9edfa9f3e610e07bcdfc1374427bb
LeyvinoBezerra <leyvinobezerra.develop@gmail.com>
2022-03-01 19:47:22 -0300

    Initial commit

---
+++ javaee/IndexBean.java
@@ -0,0 +1,15 @@
+package br.com.softblue.javaee;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named("bean")
+@RequestScoped
+public class IndexBean implements Serializable {
+
+	public String getMensagem() {
+		return "Esta � uma mensagem vinda do bean (2)!";
+	}
+}
